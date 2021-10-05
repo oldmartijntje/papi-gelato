a23925aadd332b5e3d0703b513318691fb7342e7
Martijn Van Houwelingen <oldmartijntje@gmail.com>
2021-10-05 14:23:48 +0200

    e

--- iceicebaby.py
+++ iceicebaby.py
@@ -4,18 +4,24 @@ def bolletjesSelectie():
         aantalBolletjes = input("hoe veel bolletjes wilt u?")
         try:
             aantalBolletjes = int(aantalBolletjes)
-            if aantalBolletjes > 8:
+            if aantalBolletjes >= 1 and aantalBolletjes <= 3:
+                print("")
+                loop = 0
+            elif aantalBolletjes >= 4 and aantalBolletjes <=8:
+                print(f"Dan krijgt u van mij {aantalBolletjes} bolletjes")
+                loop = 0
+            elif aantalBolletjes > 8:
                 print("Sorry, zulke grote bakjes hebben we niet")
-            loop = 0
+            else:
+                print("Sorry dat snap ik niet...")
         except:
             print("Sorry dat snap ik niet...")
     return aantalBolletjes  
-
+def bakjeOfHoorntje():
+    print()
+    
 
 print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
 aantalBolletjes = bolletjesSelectie()
-if aantalBolletjes >= 1 and aantalBolletjes <= 3:
-    print("")
-elif aantalBolletjes >= 4 and aantalBolletjes <=8:
-    print(f"Dan krijgt u van mij {aantalBolletjes} bolletjes")
+
 
