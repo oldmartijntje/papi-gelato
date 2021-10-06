6a767b40ed6ed22706ccae7312decdfd68a46de9
oldmartijntje <oldmartijntje@gmail.com>
2021-10-06 19:47:39 +0200

    "F1.5.02.O5 - Feature: Een bonnetje"

--- iceicebaby.py
+++ iceicebaby.py
@@ -1,5 +1,11 @@
 def snapUNiet():
     print("Sorry dat snap ik niet...")
+def bonnetjeCheck(nummer,naam,prijs,aankopen):
+    if aankopen[nummer] > 0:
+        print(naam+str(aankopen[nummer])+" x "+str(prijs)+"   = €" + str(aankopen[nummer]*prijs))
+        return aankopen[nummer]*prijs
+    else:
+        return 0
 def inputSelectie(mode):
     loop = 1
     while loop == 1:
@@ -41,7 +47,7 @@ def inputSelectie(mode):
     elif mode == 2:
         return again 
 def smaakjes(aantalBolletjes):
-    for x in range( 1, aantalBolletjes+1):
+    for x in range( 1, aantalBolletjes[3]+1):
         loop = 1
         while loop == 1:
             print(f"Welke smaak wilt u voor bolletje nummer {x}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?")
@@ -50,17 +56,24 @@ def smaakjes(aantalBolletjes):
                 print("Sorry dat snap ik niet...")
             else:
                 loop = 0
-def bonnetjeCreatie():
-    print()
+def bonnetjeCreatie(aankopen):
+    kosten = 0
+    print('--------["Papi Gelato"]--------\n')
+    kosten += bonnetjeCheck(0,"Bolletjes    ",1.10,aankopen)
+    kosten += bonnetjeCheck(1,"Horrentje    ",1.25,aankopen)
+    kosten += bonnetjeCheck(2,"Horrentje    ",0.75,aankopen)
+    print("                         -------+\nTotaal                   = €"+str(kosten))
+    print(aankopen)
 
-aankopen = [0,0,0]
+aankopen = [0,0,0,0]
 again = 1   
 while again == 1:
     print("Welkom bij Papi Gelato")
     print("hoe veel bolletjes wilt u?")
-    aankopen[0] += inputSelectie(1)
-    smaakjes(aankopen[0])
-    print(f"Wilt u deze {aankopen[0]} bolletje(s) in A) een hoorntje of B) een bakje?")
+    aankopen[3] = inputSelectie(1)
+    aankopen[0] += aankopen[3]
+    smaakjes(aankopen)
+    print(f"Wilt u deze {aankopen[3]} bolletje(s) in A) een hoorntje of B) een bakje?")
     bakjeOfHoorntje = inputSelectie(0)
     if bakjeOfHoorntje == "a":
         aankopen[1] += 1
@@ -70,4 +83,4 @@ while again == 1:
         bestelling = "bakje"
     print(f"Hier is uw {bestelling} met {aankopen[0]} bolletje(s). Wilt u nog meer bestellen? (Y/N)")
     again = inputSelectie(2)
-
+bonnetjeCreatie(aankopen)
